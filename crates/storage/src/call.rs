bed2b6e0277efcebdffb4c7f043e4330a328e430
Andreas Tzionis <andy0n7@gmail.com>
2023-12-24 17:35:55 +0200

    disable wasmi

--- crates/storage/src/call.rs
+++ crates/storage/src/call.rs
@@ -7,7 +7,7 @@ use sov_modules_api::{CallResponse, WorkingSet};
 use sov_modules_api::macros::CliWalletArg;
 use thiserror::Error;
 
-use wasmi::{Engine, Linker, Module, Store};
+// use wasmi::{Engine, Linker, Module, Store};
 
 use crate::ExampleModule;
 
@@ -40,53 +40,54 @@ impl<C: sov_modules_api::Context> ExampleModule<C> {
         working_set: &mut WorkingSet<C>,
     ) -> Result<sov_modules_api::CallResponse> {
 
-        let wat = r#"
-            (module
-                (export "fib" (func $fib))
-                (func $fib (; 0 ;) (param $0 i32) (result i32)
-                    (local $1 i32)
-                    (local $2 i32)
-                    (local $3 i32)
-                    (local $4 i32)
-                    (set_local $4 (i32.const 1))
-                    (block $label$0
-                        (br_if $label$0 (i32.lt_s (get_local $0) (i32.const 1)))
-                        (set_local $3 (i32.const 0))
-                        (loop $label$1
-                        (set_local $1
-                        (i32.add (get_local $3) (get_local $4))
-                    )
-                    (set_local $2 (get_local $4))
-                    (set_local $3 (get_local $4))
-                    (set_local $4 (get_local $1))
-                    (br_if $label$1 (tee_local $0 (i32.add (get_local $0) (i32.const -1)))))
-                    (return (get_local $2))
-                )
-                (i32.const 0)
-            )
-        )
-        "#;
+        // let wat = r#"
+        //     (module
+        //         (export "fib" (func $fib))
+        //         (func $fib (; 0 ;) (param $0 i32) (result i32)
+        //             (local $1 i32)
+        //             (local $2 i32)
+        //             (local $3 i32)
+        //             (local $4 i32)
+        //             (set_local $4 (i32.const 1))
+        //             (block $label$0
+        //                 (br_if $label$0 (i32.lt_s (get_local $0) (i32.const 1)))
+        //                 (set_local $3 (i32.const 0))
+        //                 (loop $label$1
+        //                 (set_local $1
+        //                 (i32.add (get_local $3) (get_local $4))
+        //             )
+        //             (set_local $2 (get_local $4))
+        //             (set_local $3 (get_local $4))
+        //             (set_local $4 (get_local $1))
+        //             (br_if $label$1 (tee_local $0 (i32.add (get_local $0) (i32.const -1)))))
+        //             (return (get_local $2))
+        //         )
+        //         (i32.const 0)
+        //     )
+        // )
+        // "#;
 
-        let wasm = wat::parse_str(wat).expect("Failed to parse_str");
+        // let wasm = wat::parse_str(wat).expect("Failed to parse_str");
 
-        let engine = Engine::default();
+        // let engine = Engine::default();
 
-        // Derived from the wasmi example: https://docs.rs/wasmi/0.29.0/wasmi/#example
-        let module = Module::new(&engine, &mut &wasm[..]).expect("Failed to create module");
-        type HostState = u32;
+        // // Derived from the wasmi example: https://docs.rs/wasmi/0.29.0/wasmi/#example
+        // let module = Module::new(&engine, &mut &wasm[..]).expect("Failed to create module");
+        // type HostState = u32;
 
-        let linker = <Linker<HostState>>::new(&engine);
-        let mut store = Store::new(&engine, 42);
-        let instance = linker
-            .instantiate(&mut store, &module)
-            .expect("failed to instantiate")
-            .start(&mut store)
-            .expect("Failed to start");
+        // let linker = <Linker<HostState>>::new(&engine);
+        // let mut store = Store::new(&engine, 42);
+        // let instance = linker
+        //     .instantiate(&mut store, &module)
+        //     .expect("failed to instantiate")
+        //     .start(&mut store)
+        //     .expect("Failed to start");
 
-        let fib = instance
-            .get_typed_func::<i32, i32>(&store, "fib")
-            .expect("Failed to get typed_func");
-        let res = fib.call(&mut store, 2).expect("Failed to call");
+        // let fib = instance
+        //     .get_typed_func::<i32, i32>(&store, "fib")
+        //     .expect("Failed to get typed_func");
+        // let res = fib.call(&mut store, 2).expect("Failed to call");
+        let res = 2;
         
         self.value.set(&res, working_set);
         working_set.add_event("set", &format!("value_set: {res:?}"));
